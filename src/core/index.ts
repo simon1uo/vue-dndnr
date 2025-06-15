0992d8f5d8fc7a36cf4d79f96adc7858397ca075
simon1uo <simon1uo@163.com>
2025-06-15 22:59:13 +0800

    refactor(DnD): remove unused core files and exports
    - Deleted AnimationManager, CustomDragInstance, EventDispatcher, and SortableManager classes to streamline the codebase.
    - Removed related exports from index files to clean up the module structure.

--- src/core/index.ts
+++
@@ -1,4 +0,0 @@
-export { AnimationManager, type AnimationOptions } from './animation-manager'
-export { CustomDragInstance } from './custom-drag-instance'
-export { EventDispatcher } from './event-dispatcher'
-export { SortableManager } from './sortable-manager'
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './components'
-export * from './core'
 export * from './hooks'
 export * from './types'
 export * from './utils'
